54b3cd7ded84f5c02c52bea79c2fbcd94037f301
Reimu Neko Miko <pinguin255@gmail.com>
2016-11-06 06:35:32 +0300

    String representation of core

--- src/main/scala/core/Measurement.scala
+++ src/main/scala/core/Measurement.scala
@@ -5,5 +5,6 @@ package core
   * Created by nekomiko on 04.11.16.
   */
 class Measurement(val id: Int, var name: String, var value: Int) {
+  override def toString = id + ": " + name + " (" + value +")"
 
 }
--- src/main/scala/core/MeasurementLog.scala
+++ src/main/scala/core/MeasurementLog.scala
@@ -7,5 +7,6 @@ import com.github.nscala_time.time.Imports._
   * Created by nekomiko on 04.11.16.
   */
 class MeasurementLog(val id: Int, val mid: Int, var date: DateTime, var message: String) {
+  override def toString = id +"(" + mid + ")  [" + date + "]  " + message
 
 }
