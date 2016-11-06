fb67d9b90135dfab3771672abda74a50a7e5fccc
Reimu Neko Miko <pinguin255@gmail.com>
2016-11-06 10:58:00 +0300

    Cosmetic fixes of initial table creation

--- src/main/scala/core/SQLiteInitializer.scala
+++ src/main/scala/core/SQLiteInitializer.scala
@@ -7,20 +7,9 @@ import java.sql.DriverManager
   */
 object SQLiteInitializer {
   def createTableIfNotExists(c: Connection, tableName: String, schema: String): Unit = {
-    val query = "SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?;"
-    val tSt = c.prepareStatement(query)
-    tSt.setString(1,tableName)
-    val rs = tSt.executeQuery()
-
-    if (rs.getInt(1) == 0){
-      val cSt = c.createStatement()
-      println("Table " + tableName + " not exists, creating new")
-      cSt.executeUpdate("CREATE TABLE IF NOT EXISTS " + tableName + " " + schema + ";")
-      cSt.close()
-    }
-    else
-      //println("Table " + tableName +" exists, OK")
-    tSt.close()
+    val cSt = c.createStatement()
+    cSt.executeUpdate("CREATE TABLE IF NOT EXISTS " + tableName + " " + schema + ";")
+    cSt.close()
   }
 
   def init(dbpath: String): Connection = {
